5a23431df4e404b4e5650479312a9c88bb189150
srabon0 <srabonemam4@gmail.com>
2023-02-07 15:28:21 +0600

    final

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json({limit: '100mb'}));
 app.set('view engine', 'ejs');
